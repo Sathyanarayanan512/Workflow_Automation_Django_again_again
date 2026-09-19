93aa1d97848e366a1b3a298e74b6c335f3ce40fa
Sathyanarayanan <bsnsathyabsn512@gmail.com>
2026-09-20 00:55:37 +0530

    Register application models in Django admin

--- app1/admin.py
+++ app1/admin.py
@@ -1,3 +1,29 @@
-from django.contrib import admin
+# from django.contrib import admin
 
-# Register your models here.
+# # Register your models here.
+from django.contrib import admin
+from .models import (
+    User,
+    Department,
+    Role,
+    Request,
+    CreatedRequest,
+    WorkflowStep,
+    TemporaryWorkflowStep,
+    ActualWorkflowStep,
+    RegularBillsRequestDetails,
+    PurchaseRequestDetails,
+    LeaveRequestDetails,
+)
+
+admin.site.register(User)
+admin.site.register(Department)
+admin.site.register(Role)
+admin.site.register(Request)
+admin.site.register(CreatedRequest)
+admin.site.register(WorkflowStep)
+admin.site.register(TemporaryWorkflowStep)
+admin.site.register(ActualWorkflowStep)
+admin.site.register(RegularBillsRequestDetails)
+admin.site.register(PurchaseRequestDetails)
+admin.site.register(LeaveRequestDetails)
